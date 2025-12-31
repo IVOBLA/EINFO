a032d671271b7bfedadfb08ffb9c752486f69045
IVOBLA <blaha540@gmail.com>
2025-12-31 11:15:16 +0100

    Add files via upload

---
+++ chatbot/Test.bat
@@ -0,0 +1,8 @@
+set OLLAMA_NO_GPU=1
+set LLM_BASE_URL=http://127.0.0.1:11434
+set LLM_CHAT_MODEL=mixtral_einfo
+set LLM_EMBED_MODEL=nomic-embed-text
+set CHATBOT_DEBUG=1
+
+cd C:\kanban\chatbot
+npm start
